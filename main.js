d54aea4456e411ee01dd0afd571e0d036b8c6083
Leyli_Alakbarova <leyaleka3@gmail.com>
2024-06-09 22:46:38 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+
+
+
+document.getElementById('my_button').addEventListener('click', ()=>{
+    
+    document.getElementById('my_button').innerText='I was clicked';
+    document.getElementById('my_button').style.width='400px';
+    document.getElementById('container').style.backgroundColor='blue';
+
+})
+document.getElementById('my_button2').addEventListener('click' ,()=>{
+    
+    document.getElementById('my_button2').innerText='I was clicked';
+    document.getElementById('my_button2').style.width='400px';
+    document.getElementById('container').style.backgroundColor='#f10b91';
+
+})
+document.getElementById('my_button3').addEventListener('click' ,()=>{
+    
+    document.getElementById('my_button3').innerText='I was clicked';
+    document.getElementById('my_button3').style.width='400px';
+    document.getElementById('container').style.backgroundColor='#d36865';
+
+})
+document.getElementById('my_button4').addEventListener('click' ,()=>{
+    
+    document.getElementById('my_button4').innerText='I was clicked';
+    document.getElementById('my_button4').style.width='400px';
+    document.getElementById('container').style.backgroundColor='#208517';
+
+})
+document.getElementById('my_button5').addEventListener('click' ,()=>{
+    
+    document.getElementById('my_button5').innerText='I was clicked';
+    document.getElementById('my_button5').style.width='400px';
+    document.getElementById('container').style.backgroundColor='dodgerblue';
+
+})
+document.getElementById('my_button6').addEventListener('click' ,()=>{
+    
+    document.getElementById('my_button6').innerText='I was clicked';
+    document.getElementById('my_button6').style.width='400px';
+    document.getElementById('container').style.backgroundColor='darkviolet';
+
+})
+document.getElementById('my_button7').addEventListener('click' ,()=>{
+    
+    document.getElementById('my_button7').innerText='I was clicked';
+    document.getElementById('my_button7').style.width='400px';
+    document.getElementById('container').style.backgroundColor='violet';
+
+})
+
+ 
